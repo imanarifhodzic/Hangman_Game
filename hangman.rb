59363fcf4b3e305722273dfbaf7a0d155c60b728
imanarifhodzic <iman.arifhodzic@stu.ibu.edu.ba>
2025-01-07 22:38:35 +0100

    Some bug fixes

--- hangman.rb
+++ hangman.rb
@@ -1,74 +1,46 @@
-require 'io/console'
-require 'yaml'
-require 'colorize'
-
-def reload_scoreboard
-  if File.exist?('scoreboard_save.yml')
-    $scoreboard = YAML.load_file('scoreboard_save.yml')
-    $scoreboard = {} unless $scoreboard.is_a?(Hash)
+def load_game
+  if File.exist?("saved_game.yml")
+    data = YAML.load_file("saved_game.yml")
+    @current_word = data[:current_word]
+    @masked_word = data[:masked_word]
+    @attempts = data[:attempts]
+    @guessed_letters = data[:guessed_letters]
+    @current_player = data[:current_player]
+    puts "Game loaded successfully!".colorize(:green)
+    true
   else
-    $scoreboard = {}
+    puts "No saved game found!".colorize(:red)
+    false
   end
 end
 
-def save_scoreboard
-  File.open('scoreboard_save.yml', 'w') { |file| file.write($scoreboard.to_yaml) }
+
+def initialize_game
+  @masked_word = '_' * @current_word.length
+  @attempts = 6
+  @guessed_letters = []
 end
 
-reload_scoreboard
 
-WORDS = ['elektrifikacija', 'matematika', 'sportista', 'fantazija', 'astronaut']
-$chosen_word = ''
-$masked_word = ''
-$remaining_attempts = 6
-$guessed_letters = []
-$current_player = ''
-
-def draw_hangman(mistakes)
-  hangman = "   +---+\n       |\n       |\n       |\n      ===\n"
-
-  case mistakes
-  when 1
-    hangman = "   +---+\n   O   |\n       |\n       |\n      ===\n".yellow
-  when 2
-    hangman = "   +---+\n   O   |\n   |   |\n       |\n      ===\n".cyan
-  when 3
-    hangman = "   +---+\n   O   |\n  /|   |\n       |\n      ===\n".cyan
-  when 4
-    hangman = "   +---+\n   O   |\n  /|\  |\n       |\n      ===\n".cyan
-  when 5
-    hangman = "   +---+\n   O   |\n  /|\  |\n  /    |\n      ===\n".cyan
-  when 6
-    hangman = "   +---+\n   O   |\n  /|\  |\n  / \  |\n      ===\n".cyan
-  end
-
-  puts hangman
+def print_hangman(wrong_guesses)
+  hangman = [
+    "   +---+\n       |\n       |\n       |\n      ===",
+    "   +---+\n   O   |\n       |\n       |\n      ===",
+    "   +---+\n   O   |\n   |   |\n       |\n      ===",
+    "   +---+\n   O   |\n  /|   |\n       |\n      ===",
+    "   +---+\n   O   |\n  /|\\  |\n       |\n      ===",
+    "   +---+\n   O   |\n  /|\\  |\n  /    |\n      ===",
+    "   +---+\n   O   |\n  /|\\  |\n  / \\  |\n      ==="
+  ]
+  puts hangman[wrong_guesses].colorize(:yellow)
 end
 
-def persist_game(filename)
-  game_state = {
-    word: $chosen_word,
-    masked_word: $masked_word,
-    attempts: $remaining_attempts,
-    guessed_letters: $guessed_letters,
-    current_player: $current_player
-  }
-  File.write(filename, YAML.dump(game_state))
-  puts 'Game saved!'
-end
 
-def reload_game(filename)
-  game_state = YAML.load_file(filename)
-  $chosen_word = game_state[:word]
-  $masked_word = game_state[:masked_word]
-  $remaining_attempts = game_state[:attempts]
-  $guessed_letters = game_state[:guessed_letters]
-  $current_player = game_state[:current_player]
-  puts 'Game loaded!'
+def display_menu
+  puts "\n~~~ Hangman Menu ~~~".colorize(:magenta)
+  puts "1. Start New Game".colorize(:cyan)
+  puts "2. Resume Saved Game".colorize(:cyan)
+  puts "3. View Scoreboard".colorize(:cyan)
+  puts "4. Exit".colorize(:cyan)
+  print "Your choice: ".colorize(:yellow)
 end
-
-def persist_scoreboard(filename)
-  File.write(filename, YAML.dump(scoreboard: $scoreboard))
-  puts 'Scoreboard saved!'
-end
-
